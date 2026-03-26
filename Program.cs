4685f1cd0605dbaa7f08e9aab87ee5dc18af8f85
gidalti <gidalti.lopes-terceiro@cea.com.br>
2026-03-26 17:19:32 -0300

    discord

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World ARM64 🚀");
+app.MapGet("/", () => "Hello World ARM64");
 
-app.Run();
+app.Run("http://0.0.0.0:80");
